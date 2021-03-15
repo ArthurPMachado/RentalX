d93e739b65205b356e1e2753718616fb0d7a9658
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-03-15 18:05:18 -0300

    Chore: add prettier and plugins to connect with eslint

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,6 @@ import express from 'express';
 
 const server = express();
 
-server.get("/", (request, response) => {
-  return response.json({ message: "Hello World" })
-})
+server.get('/', (request, response) => response.json({ message: 'Hello World' }));
 
 server.listen(3030);
