c8463a6c45dbf1f2e6608cf2d1735b9f3c933137
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-03-16 15:01:54 -0300

    Feat: add validation to not create duplicate categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -8,9 +8,21 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: "Category Already Exists" });
+  }
+
   categoriesRepository.create({ name, description });
 
   return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+  const listAll = categoriesRepository.listAll();
+
+  return response.json(listAll);
 });
 
 export default categoriesRoutes;
