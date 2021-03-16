d8bee05aacff2bb2dc402e54ff82490f2ef061c2
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-03-16 17:31:40 -0300

    Feat: add repository interface with LSP concept from SOLID

--- src/services/CreateCategoryService.ts
+++ src/services/CreateCategoryService.ts
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import CategoriesRepository from "../repositories/CategoriesRepository";
+import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
 
 interface IRequest {
   name: string;
@@ -7,7 +7,7 @@ interface IRequest {
 }
 
 class CreateCategoryService {
-  constructor(private categoriesRepository: CategoriesRepository) { }
+  constructor(private categoriesRepository: ICategoriesRepository) { }
 
   execute({ name, description }: IRequest): void {
     const categoryAlreadyExists = this.categoriesRepository.findByName(name);
