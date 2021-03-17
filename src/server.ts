365b452111caa8151b437fa535965728af1dbbef
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-03-17 16:56:56 -0300

    Feat: configure swagger

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import router from "./routes";
+import swaggerFile from "./swagger.json";
 
 const server = express();
 
 server.use(express.json());
+
+server.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 server.use(router);
 
