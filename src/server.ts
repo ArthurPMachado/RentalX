a30856dc0b96bec44299f4522a6b72ed578d9929
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-03-17 01:21:24 -0300

    Feat: add specifications routes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from "express";
 
 import categoriesRoutes from "./routes/categories.routes";
+import specificationRoutes from "./routes/specifications.routes";
 
 const server = express();
 
 server.use(express.json());
 
 server.use("/categories", categoriesRoutes);
+server.use("/specifications", specificationRoutes);
 
 server.listen(3030, () => console.log("Server is running"));
