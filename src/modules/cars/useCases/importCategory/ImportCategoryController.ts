623edf997972db083cb412253720dacb7a74ebda
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-03-26 00:20:58 -0300

    Refactor: use dependency inversion with tsyringe on specifications

--- src/modules/cars/useCases/importCategory/ImportCategoryController.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -6,12 +6,12 @@ import ImportCategoryUseCase from "./ImportCategoryUseCase";
 
 class ImportCategoryController {
 
-  handle(request: Request, response: Response): Response {
+  async handle(request: Request, response: Response): Promise<Response> {
     const { file } = request;
 
     const importCategoryUseCase = container.resolve(ImportCategoryUseCase);
 
-    importCategoryUseCase.execute(file);
+    await importCategoryUseCase.execute(file);
 
     return response.send();
   }
--- src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
@@ -1,12 +1,17 @@
+/* eslint-disable prettier/prettier */
+import { inject, injectable } from "tsyringe";
+
 import Category from "../../models/Category";
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
+@injectable()
 class ListCategoriesUseCase {
-  // eslint-disable-next-line prettier/prettier
-  constructor(private categoriesRepository: ICategoriesRepository) { }
+  constructor(
+    @inject("CategoriesRepository")
+    private categoriesRepository: ICategoriesRepository) { }
 
-  execute(): Category[] {
-    const categories = this.categoriesRepository.listAll();
+  async execute(): Promise<Category[]> {
+    const categories = await this.categoriesRepository.listAll();
 
     return categories;
   }
