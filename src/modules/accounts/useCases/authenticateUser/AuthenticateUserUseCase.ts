c7be02f56a7825d989f5cf354754206ca2f612d7
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-03-29 01:17:21 -0300

    Feat: add user authentication with jwt and bcrypt

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,61 @@
+/* eslint-disable prettier/prettier */
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import IUsersRepository from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string,
+    email: string
+  },
+  token: string
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) { }
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    // Usuário existe
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error("Email or password incorrect");
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    // Senha incorreta
+    if (!passwordMatch) {
+      throw new Error("Email or password incorrect");
+    }
+
+    // Gerar jwt
+    const token = sign({}, "a2b55458cdd971341a7dd1e3f394e575", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    const tokenReturn: IResponse = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email
+      }
+    }
+
+    return tokenReturn;
+  }
+}
+
+export default AuthenticateUserUseCase;
