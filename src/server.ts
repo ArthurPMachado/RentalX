6e95e0975665fc235be0c9e1d24f4ba2d3cfaa0c
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-03-29 20:01:05 -0300

    Feat: add error middleware to server

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import "reflect-metadata";
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import swaggerUi from "swagger-ui-express";
 
 import "./database";
 import "./shared/container";
 
+import AppError from "./errors/AppError";
 import router from "./routes";
 import swaggerFile from "./swagger.json";
 
@@ -15,5 +16,20 @@ server.use(express.json());
 server.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 server.use(router);
+
+server.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "Error",
+      message: `Internal server error - ${err.message}`,
+    });
+  }
+);
 
 server.listen(3030, () => console.log("Server is running"));
