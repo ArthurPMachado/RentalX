2ce56a1e3d1bca0638c81b9b18e3132893244010
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-03-29 19:06:21 -0300

    Feat: add middleware to ensure authentication

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -4,6 +4,7 @@ import User from "../models/User";
 interface IUsersRepository {
   create(data: ICreateUserDTO): Promise<void>;
   findByEmail(email: string): Promise<User | undefined>;
+  findById(id: string): Promise<User>;
 }
 
 export default IUsersRepository;
