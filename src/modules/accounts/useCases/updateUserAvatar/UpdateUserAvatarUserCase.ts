bd55b772aa688797ce09459272614d1d78f9bae8
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-03-30 12:46:12 -0300

    Feat: delete old avatars

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUserCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUserCase.ts
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
 import { inject, injectable } from "tsyringe";
 
+import { deleteFile } from "../../../../utils/file";
 import IUsersRepository from "../../repositories/IUsersRepository";
 
 interface IRequest {
@@ -17,6 +18,10 @@ class UpdateUserAvatarUseCase {
 
   async execute({ user_id, avatar_file }: IRequest): Promise<void> {
     const user = await this.usersRepository.findById(user_id);
+
+    if (user.avatar) {
+      await deleteFile(`./tmp/avatar/${user.avatar}`);
+    }
 
     user.avatar = avatar_file;
 
