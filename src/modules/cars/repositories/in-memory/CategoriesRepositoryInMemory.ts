19173a732133792e71d836d4788309daa15bec29
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-03-31 23:28:40 -0300

    Feat: add in memory category repository

---
+++ src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,34 @@
+import Category from "../../models/Category";
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from "../ICategoriesRepository";
+
+class CategoriesRepositoryInMemory implements ICategoriesRepository {
+  categories: Category[] = [];
+
+  async findByName(name: string): Promise<Category> {
+    const category = await this.categories.find(
+      (category) => category.name === name
+    );
+    return category;
+  }
+
+  async listAll(): Promise<Category[]> {
+    const categories = await this.categories;
+    return categories;
+  }
+
+  async create({ name, description }: ICreateCategoryDTO): Promise<void> {
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+    });
+
+    this.categories.push(category);
+  }
+}
+
+export default CategoriesRepositoryInMemory;
