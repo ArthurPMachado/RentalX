3c671e1b35f79a47d237934e0824755495d25ee0
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-04-02 17:41:14 -0300

    Refactor: change repositories to put inside infra folder

---
+++ src/modules/cars/infra/typeorm/repositories/CategoriesRepository.ts
@@ -0,0 +1,39 @@
+import { getRepository, Repository } from "typeorm";
+
+import Category from "@modules/cars/infra/typeorm/models/Category";
+
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from "../../../repositories/ICategoriesRepository";
+
+class CategoriesRepository implements ICategoriesRepository {
+  private repository: Repository<Category>;
+
+  constructor() {
+    this.repository = getRepository(Category);
+  }
+
+  async create({ name, description }: ICreateCategoryDTO): Promise<void> {
+    const category = this.repository.create({
+      name,
+      description,
+    });
+
+    await this.repository.save(category);
+  }
+
+  async listAll(): Promise<Category[]> {
+    const categories = await this.repository.find();
+
+    return categories;
+  }
+
+  async findByName(name: string): Promise<Category> {
+    const category = await this.repository.findOne({ name });
+
+    return category;
+  }
+}
+
+export default CategoriesRepository;
