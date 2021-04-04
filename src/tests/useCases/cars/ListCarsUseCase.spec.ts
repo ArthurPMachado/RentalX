03fddeeb8ced5526e952124042c9fbca0ea56c49
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-04-04 17:40:21 -0300

    Test: list cars by name and category using TDD

--- src/tests/useCases/cars/ListCarsUseCase.spec.ts
+++ src/tests/useCases/cars/ListCarsUseCase.spec.ts
@@ -26,7 +26,7 @@ describe("List Cars", () => {
     expect(cars).toEqual([car]);
   });
 
-  it("Should be able to list all available cars by name", async () => {
+  it("Should be able to list all available cars by brand", async () => {
     const car = await carsRepositoryInMemory.create({
       name: "Car1",
       description: "Car description",
@@ -39,6 +39,42 @@ describe("List Cars", () => {
 
     const cars = await listCarsUseCase.execute({
       brand: "Car_brand_test",
+    });
+
+    expect(cars).toEqual([car]);
+  });
+
+  it("Should be able to list all available cars by name", async () => {
+    const car = await carsRepositoryInMemory.create({
+      name: "Car3",
+      description: "Car description",
+      daily_rate: 140.0,
+      license_plate: "TUV-9876",
+      fine_amount: 100,
+      brand: "Car_brand_test",
+      category_id: "category_id",
+    });
+
+    const cars = await listCarsUseCase.execute({
+      name: "Car3",
+    });
+
+    expect(cars).toEqual([car]);
+  });
+
+  it("Should be able to list all available cars by category", async () => {
+    const car = await carsRepositoryInMemory.create({
+      name: "Car3",
+      description: "Car description",
+      daily_rate: 140.0,
+      license_plate: "TUV-9876",
+      fine_amount: 100,
+      brand: "Car_brand_test",
+      category_id: "12345",
+    });
+
+    const cars = await listCarsUseCase.execute({
+      category_id: "12345",
     });
 
     expect(cars).toEqual([car]);
