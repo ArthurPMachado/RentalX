91cf565fa3c55c1bfdca239d14e43776cc8399f9
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-04-04 22:26:01 -0300

    Feat: implements add specifications to car

--- src/modules/cars/useCases/createCarSpecifiation/CreateCarSpecificationUseCase.ts
+++ src/modules/cars/useCases/createCarSpecifiation/CreateCarSpecificationUseCase.ts
@@ -11,11 +11,13 @@ interface IRequest {
   specifications_id: string[];
 }
 
-// @injectable()
+@injectable()
 class CreateCarSpecificationUseCase {
   constructor(
-    // @inject("CarsRepository")
+    @inject("CarsRepository")
     private carsRepository: ICarsRepository,
+
+    @inject("SpecificationsRepository")
     private specificationsRepository: ISpecificationsRepository
   ) { }
 
