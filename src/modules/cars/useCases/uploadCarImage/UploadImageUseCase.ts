682c964ae205c0a9d9989f23b39e516c485a1103
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-04-05 00:01:30 -0300

    Feat: implements car image upload

--- src/modules/cars/useCases/uploadCarImage/UploadImageUseCase.ts
+++ src/modules/cars/useCases/uploadCarImage/UploadImageUseCase.ts
@@ -1,3 +1,27 @@
-class UploadCarImageUseCase { }
+/* eslint-disable prettier/prettier */
+import { inject, injectable } from "tsyringe";
 
-export default UploadImageUseCase;
+import ICarsImagesRepository from "@modules/cars/repositories/ICarsImagesRepository";
+
+interface IRequest {
+  car_id: string;
+  images_name: string[];
+}
+
+@injectable()
+class UploadCarImageUseCase {
+  constructor(
+    @inject("CarsImagesRepository")
+    private carsImageRepository: ICarsImagesRepository
+  ) {
+
+  }
+
+  async execute({ car_id, images_name }: IRequest): Promise<void> {
+    images_name.forEach(async (image) => {
+      await this.carsImageRepository.create(car_id, image);
+    })
+  }
+}
+
+export default UploadCarImageUseCase;
--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,17 +1,23 @@
 import { Router } from "express";
+import multer from "multer";
 
+import uploadConfig from "@config/upload";
 import CreateCarController from "@modules/cars/useCases/createCar/CreateCarController";
 import CreateCarSpecificationController from "@modules/cars/useCases/createCarSpecifiation/CreateCarSpecificationController";
 import ListAvailableCarsController from "@modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
+import UploadCarImageController from "@modules/cars/useCases/uploadCarImage/UploadCarImageController";
 
 import ensureAdmin from "../middlewares/ensureAdmin";
 import ensureAuthenticated from "../middlewares/ensureAuthenticated";
 
 const carsRoutes = Router();
 
+const uploadCarImage = multer(uploadConfig.upload("./tmp/cars"));
+
 const createCarController = new CreateCarController();
 const listAvailableCarsController = new ListAvailableCarsController();
 const createCarSpecificationController = new CreateCarSpecificationController();
+const uploadCarImageController = new UploadCarImageController();
 
 carsRoutes.post(
   "/",
@@ -27,6 +33,14 @@ carsRoutes.post(
   ensureAuthenticated,
   ensureAdmin,
   createCarSpecificationController.handle
+);
+
+carsRoutes.post(
+  "/images/:id",
+  ensureAuthenticated,
+  ensureAdmin,
+  uploadCarImage.array("images"),
+  uploadCarImageController.handle
 );
 
 export default carsRoutes;
