1f169b90baafded079f55cb5c7baa5d9a6ba8590
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-04-11 13:37:04 -0300

    Feat: implements reset password

--- src/modules/accounts/repositories/IUserTokensRepository.ts
+++ src/modules/accounts/repositories/IUserTokensRepository.ts
@@ -12,6 +12,7 @@ interface IUserTokensRepository {
     refresh_token: string
   ): Promise<UserTokens>;
   deleteById(id: string): Promise<void>;
+  findByRefreshToken(refresh_token: string): Promise<UserTokens>;
 }
 
 export default IUserTokensRepository;
