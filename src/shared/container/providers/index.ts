0fca16a1f2d4727b67f247f011d3f3cbd4ccea4f
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-04-13 19:25:21 -0300

    Feat: add S3 provider and save images to S3

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -5,6 +5,7 @@ import DayjsDateProvider from "./DateProvider/implementations/DayjsDateProvide";
 import IMailProvider from "./MailProvider/IMailProvider";
 import EtherealMailProvider from "./MailProvider/implementations/EtherealMailProvider";
 import LocalStorageProvider from "./StorageProvider/implementations/LocalStorageProvide";
+import S3StorageProvider from "./StorageProvider/implementations/S3StorageProvider";
 import IStorageProvider from "./StorageProvider/IStorageProvider";
 
 container.registerSingleton<IDateProvider>(
@@ -19,5 +20,5 @@ container.registerInstance<IMailProvider>(
 
 container.registerSingleton<IStorageProvider>(
   "StorageProvider",
-  LocalStorageProvider
+  S3StorageProvider
 );
