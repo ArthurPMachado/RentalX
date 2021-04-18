74e773d1de8c924cd3ef5c2a9880cdb029721f50
Arthur Pereira Machado <machadoparthur1@gmail.com>
2021-04-18 20:23:30 -0300

    Feat: add sentry to monitor errors in production

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -5,9 +5,12 @@ import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
+import upload from "@config/upload";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
+
 import "../../container";
 
-import upload from "@config/upload";
 import AppError from "@shared/errors/AppError";
 
 import swaggerFile from "../../../swagger.json";
@@ -20,6 +23,21 @@ const server = express();
 
 server.use(rateLimiter);
 
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app: server }),
+  ],
+
+  tracesSampleRate: 1.0,
+});
+
+server.use(Sentry.Handlers.requestHandler());
+server.use(Sentry.Handlers.tracingHandler());
+
 server.use(express.json());
 
 server.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
@@ -28,6 +46,8 @@ server.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
 server.use(cors());
 server.use(router);
+
+server.use(Sentry.Handlers.errorHandler());
 
 server.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
